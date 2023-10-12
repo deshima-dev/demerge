e3fd6df3f22460d5a1cf3887c6e48f98d9fb711e
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2023-10-12 09:25:29 +0000

    #16 Move merge scripts into dmerge package

--- dmerge/__init__.py
+++ dmerge/__init__.py
@@ -1,1 +1,20 @@
+__all__ = [
+    "calc_resonance_params",
+    "dmerge",
+    "make_divided_data",
+    "make_reduced_fits",
+    "merge_function",
+    "merge_to_dems",
+    "plot",
+]
 __version__ = "2.0.0"
+
+
+# submodules
+from . import calc_resonance_params
+from . import dmerge
+from . import make_divided_data
+from . import make_reduced_fits
+from . import merge_function
+from . import merge_to_dems
+from . import plot

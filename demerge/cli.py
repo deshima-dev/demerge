d9bd83271dced52ad26e7c54da42b4b9e17f1287
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2023-10-29 12:27:29 +0000

    #56 Rename former CLI (demerge →demerge-sh)

--- demerge/cli.py
+++ demerge/cli.py
@@ -1,2 +1,14 @@
 __all__ = []
 
+
+# standard library
+from pathlib import Path
+from subprocess import run
+from sys import argv
+
+
+def main_sh() -> None:
+    """Run cli.sh (formerly run.sh)."""
+
+    cmd = Path(__file__).parent / "cli.sh"
+    run([cmd, *argv[1:]])

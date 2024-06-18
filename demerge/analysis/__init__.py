d0af12950a46f55b2fabdd5870a96da9864fe9cc
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2024-06-18 10:51:19 +0000

    #110 Add CLI function for analysis

--- demerge/analysis/__init__.py
+++ demerge/analysis/__init__.py
@@ -1,13 +1,35 @@
 __all__ = ["analyze"]
 
 
+# standard library
+from pathlib import Path
+from subprocess import run
+from tempfile import TemporaryDirectory
+
+
 # dependencies
 from fire import Fire
 
 
-def analyze() -> None:
-    """Analyze a TOD FITS and create a reduced FITS."""
-    ...
+# constants
+SCRIPTS = Path(__file__).parent / "utils" / "scripts" / "aste"
+
+
+def analyze(data_dir: str, output_dir: str) -> None:
+    """Analyze a DESHIMA TOD FITS and create a reduced FITS.
+
+    Args:
+        data_dir: Path of raw data directory (e.g. ``cosmos_20240101000000``).
+        output_dir: Path of output directory (e.g. ``analysis_20240101000000``).
+
+    """
+    data_dir = Path(data_dir).resolve()
+    output_dir = Path(output_dir).resolve()
+
+    with TemporaryDirectory() as work_dir:
+        run(["python", SCRIPTS / "Configure.py", data_dir, output_dir], cwd=work_dir)
+        run(["python", SCRIPTS / "FitSweep.py"], cwd=work_dir)
+        run(["python", SCRIPTS / "SaveFits.py"], cwd=work_dir)
 
 
 def cli() -> None:

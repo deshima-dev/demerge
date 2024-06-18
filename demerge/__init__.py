a62e7cb990188c527e081ed3607b328446b7c7b0
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2024-06-18 02:39:56 +0000

    #110 Remove unused modules

--- demerge/__init__.py
+++ demerge/__init__.py
@@ -1,23 +1,10 @@
-__all__ = [
-    "calc_resonance_params",
-    "demerge",
-    "make_divided_data",
-    "make_reduced_fits",
-    "merge_function",
-    "merge_to_dems",
-    "plot",
-]
+__all__ = ["merge_function", "merge_to_dems"]
 __version__ = "2.13.0"
 
 
 # submodules
-from . import calc_resonance_params
-from . import demerge
-from . import make_divided_data
-from . import make_reduced_fits
 from . import merge_function
 from . import merge_to_dems
-from . import plot
 
 
 def main() -> None:

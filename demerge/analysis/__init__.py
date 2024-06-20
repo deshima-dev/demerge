6249d8929f1b49316d0c45ac2e85ce792706df9f
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2024-06-20 07:09:15 +0000

    #116 Return path of reduced FITS

--- demerge/analysis/__init__.py
+++ demerge/analysis/__init__.py
@@ -15,21 +15,38 @@ from fire import Fire
 SCRIPTS = Path(__file__).parent / "utils" / "scripts" / "aste"
 
 
-def analyze(data_dir: str, output_dir: str) -> None:
+def analyze(data_dir: Path, output_dir: Path) -> Path:
     """Analyze a DESHIMA TOD FITS and create a reduced FITS.
 
     Args:
         data_dir: Path of raw data directory (e.g. ``cosmos_20240101000000``).
         output_dir: Path of output directory (e.g. ``analysis_20240101000000``).
 
+    Returns:
+        Path of the created reduced FITS.
+
     """
     data_dir = Path(data_dir).resolve()
     output_dir = Path(output_dir).resolve()
 
     with TemporaryDirectory() as work_dir:
-        run(["python", SCRIPTS / "Configure.py", data_dir, output_dir], cwd=work_dir)
-        run(["python", SCRIPTS / "FitSweep.py"], cwd=work_dir)
-        run(["python", SCRIPTS / "SaveFits.py"], cwd=work_dir)
+        run(
+            ["python", SCRIPTS / "Configure.py", data_dir, output_dir],
+            capture_output=True,
+            cwd=work_dir,
+        )
+        run(
+            ["python", SCRIPTS / "FitSweep.py"],
+            capture_output=True,
+            cwd=work_dir,
+        )
+        run(
+            ["python", SCRIPTS / "SaveFits.py"],
+            capture_output=True,
+            cwd=work_dir,
+        )
+
+    return list(output_dir.glob("reduced_*.fits"))[0]
 
 
 def cli() -> None:

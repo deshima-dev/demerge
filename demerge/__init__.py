0c4a36701ce4bd27f311d5483bb1ba8bfa43dc3b
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2024-07-17 14:17:14 +0000

    #147 Update demerge function

--- demerge/__init__.py
+++ demerge/__init__.py
@@ -6,7 +6,7 @@ __version__ = "3.1.0"
 from contextlib import contextmanager
 from logging import DEBUG, basicConfig, getLogger
 from pathlib import Path
-from typing import Any
+from typing import Any, Literal
 
 
 # dependencies
@@ -15,9 +15,8 @@ from . import merge, reduce
 
 
 # constants
-DEFAULT_DDB_FILE = Path(__file__).parent / "data" / "ddb_20231123.fits.gz"
-DEFAULT_OFFSET_TIME_ANTENNA = 20  # ms
 LOGGER = getLogger(__name__)
+PACKAGE_DATA = Path(__file__).parent / "data"
 
 
 @contextmanager
@@ -37,15 +36,18 @@ def demerge(
     obsid: str,
     /,
     *,
+    # data paths
     data_dir: Path = Path(),
     dems_dir: Path = Path(),
     reduced_dir: Path = Path(),
-    ddb: Path = DEFAULT_DDB_FILE,
+    ddb: Path = PACKAGE_DATA / "ddb_20231123.fits.gz",
+    # merge options
+    measure: Literal["df/f", "brightness"] = "df/f",
     overwrite: bool = False,
     debug: bool = False,
-    **merge_options: Any,
+    **options: Any,
 ) -> Path:
-    """Run reduce and merge commands to create a single DEMS file.
+    """Run reduce and merge commands to create a single DEMS.
 
     Args:
         obsid: Observation ID (YYYYmmddHHMMSS).
@@ -56,13 +58,14 @@ def demerge(
         reduced_dir: Path where reduced data directory will be placed,
             i.e. expecting ``${reduced_dir}/reduced_YYYYmmddHHMMSS``.
         ddb: Path of DDB (DESHIMA database) file.
+        measure: Measure of the DEMS (either df/f or brightness).
         overwrite: If True, reduced data directory and merged DEMS file
             will be overwritten even if they exist.
         debug: If True, detailed logs for debugging will be printed.
-        **merge_options: Other merge options for the merge command.
+        **options: Other merge options for the merge command.
 
     Returns:
-        Path of the merged DEMS file.
+        Path of the merged DEMS.
 
     """
     with set_logger(debug):
@@ -86,49 +89,48 @@ def demerge(
     if (dems := dems_dir_ / f"dems_{obsid}.zarr.zip").exists():
         raise FileExistsError(dems)
 
-    if not ddb.exists():
-        raise FileNotFoundError(ddb)
-
     if not (corresp := data_dir_ / "kid_corresp.json").exists():
         raise FileNotFoundError(corresp)
+
+    if not ddb.exists():
+        raise FileNotFoundError(ddb)
 
     if not (obsinst := data_dir_ / f"{obsid}.obs").exists():
         raise FileNotFoundError(obsinst)
 
     if not (antenna := data_dir_ / f"{obsid}.ant").exists():
-        raise FileNotFoundError(antenna)
-
-    if not (skychop := data_dir_ / f"{obsid}.skychopper.dat.xz").exists():
-        raise FileNotFoundError(skychop)
-
-    if not (weather := data_dir_ / f"{obsid}.wea").exists():
-        raise FileNotFoundError(weather)
-
-    if not (misti := data_dir_ / f"{obsid}.misti").exists():
-        misti = None
+        antenna = None
 
     if not (cabin := data_dir_ / f"{obsid}.cabin").exists():
         cabin = None
 
-    merge_options = {
-        "offset_time_antenna": DEFAULT_OFFSET_TIME_ANTENNA,
-        **merge_options,
-    }
+    if not (misti := data_dir_ / f"{obsid}.misti").exists():
+        misti = None
 
-    merge.merge(
+    if not (skychop := data_dir_ / f"{obsid}.skychopper.dat.xz").exists():
+        skychop = None
+
+    if not (weather := data_dir_ / f"{obsid}.wea").exists():
+        weather = None
+
+    return merge.merge(
         dems,
-        ddb=ddb,
+        # required datasets
         corresp=corresp,
-        readout=readout,
+        ddb=ddb,
         obsinst=obsinst,
+        readout=readout,
+        # optional datasets
         antenna=antenna,
+        cabin=cabin,
+        misti=misti,
         skychop=skychop,
         weather=weather,
-        misti=misti,
-        cabin=cabin,
+        # merge options
+        measure=measure,
         overwrite=overwrite,
         debug=debug,
-        **merge_options,
+        **options,
     )
 
 
